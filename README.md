e1abf9a178cc0386fe16a281a19ad92735149a60
Anton Tsviatkou <a.tsviatkou@gmail.com>
2017-03-19 22:51:26 -0700

    Add vagrant setup into readme

--- README.md
+++ README.md
@@ -4,3 +4,14 @@
 Sample package to show how to test REST web service/API [for example Github API]
 using node JS
 
+# Vagrant setup
+You can run test in a virtual box, using vagrant:
+```sh
+vagrant plugin install vagrant-cachier
+vagrant up
+vagrant ssh
+GITHUB_TOKEN="your-api-token" npm test
+```
+
+To obtain API token to go: 
+[Your Github Settings](https://github.com/settings/tokens) and create a token with `public_repo` scope
--- provision/nodejs.sh
+++ provision/nodejs.sh
@@ -1,21 +1,16 @@
 #!/usr/bin/env bash
 
-which node 1>/dev/null 2>&1
+which npm 1>/dev/null 2>&1
 if [ $? -ne 0 ];then
     echo "Update information about current debian packages..."
     sudo apt-get update
-    echo "Installing node JS..."
-    sudo apt-get install -y nodejs
-fi;
-which npm 1>/dev/null 2>&1
-if [ $? -ne 0 ];then
     echo "Installing node package manager..."
-    sudo apt-get install -y npm
+    sudo apt-get install -y npm nodejs-legacy
 fi;
 
 cd /vagrant-src && echo "Installing project dependencies..." && npm install
 
 # for convenience go to directory with attached source
-if [ $? -eq 0 ] && [ ! -e "$HOME/.bash_login" ];then
-    echo "cd /vagrant-src" > "$HOME/.bash_login"
+if [ ! -e "/home/ubuntu/.bash_login" ];then
+    echo "cd /vagrant-src || echo 'Cannot cd into source directory'" > "/home/ubuntu/.bash_login"
 fi;
